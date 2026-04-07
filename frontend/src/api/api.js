34fcb25a179a38d7967fecc9b8b2952dfa9a3c23
Adithya <adithyavarun95@gmail.com>
2026-04-07 18:50:04 +0530

    url base changes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,19 +1,21 @@
 import axios from "axios";
 
+const api = "http://localhost:5000/api/";
+
 export const registerUser = async (data) => {
-  const res = await axios.post("http://localhost:5000/api/auth/signup", data);
+  const res = await axios.post(`${api}auth/signup`, data);
   return res.data;
 };
 
 export const loginUser = async (data) => {
-  const res = await axios.post("http://localhost:5000/api/auth/signin", data, {
+  const res = await axios.post(`${api}auth/signin`, data, {
     withCredentials: true,
   });
   return res.data;
 };
 
 export const logoutUser = async () => {
-  const res = await axios.get("http://localhost:5000/api/auth/signout", {
+  const res = await axios.get(`${api}auth/signout`, {
     withCredentials: true,
   });
   return res.data;
