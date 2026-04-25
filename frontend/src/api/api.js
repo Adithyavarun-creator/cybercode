dae7f1e13926830035a6c290d4f58d84e7af992b
Adithya <adithyavarun95@gmail.com>
2026-04-25 12:48:34 +0530

    url in frontend update

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // const api = "http://localhost:5000/api/";
 
 //cybercode-n2cp.onrender.com/
-const api = "https://cybercode-n2cp.onrender.com/";
+const api = "https://cybercode-n2cp.onrender.com/api/";
 
 export const registerUser = async (data) => {
   const res = await axios.post(`${api}auth/signup`, data);
